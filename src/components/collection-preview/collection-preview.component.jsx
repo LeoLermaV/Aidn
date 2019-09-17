c07f791ec96ce78aed7475f4dca334869c76b90d
Leo <leolermav@gmail.com>
2019-09-17 22:03:29 +1200

    added header and updated styles in homepage

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,16 +8,14 @@ const CollectionPreview = ({ title, items }) => (
 
 	<div className='collection-preview'>
 	 <h1 className="title">{title.toUpperCase()} </h1>
-	 	<div className="preview"> 
+	 <div className="preview"> 
 	 	{items
 	 		.filter((item, idx) => idx < 4)
 	 		.map (({id,...otherItemProps} ) => (
-	 				<CollectionItem key={id} {...otherItemProps}/>
+	 			<CollectionItem key={id} {...otherItemProps}/>
 	 			))}
-	 	</div>
-	</div>
-
- );
+	 </div>
+	</div>);
 
 
 export default CollectionPreview;
