a8d24b2939650aa47fdc05f99b7c8a3a601f81f6
Leo <leolermav@gmail.com>
2019-10-01 21:48:24 +1300

    polishing details and switching covers

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
 	sections: [
   {
     title: 'hats',
-    imageUrl: `https://i.ibb.co/cvpntL1/hats.png`,
+    imageUrl: `https://i.imgur.com/ZDBLkII.jpg`,
     id: 1,
     linkUrl: 'shop/hats'
   },
   {
     title: 'jackets',
-    imageUrl: `https://i.ibb.co/px2tCc3/jackets.png`,
+    imageUrl: `https://i.imgur.com/gTW7GNM.jpg`,
     id: 2,
     linkUrl: 'shop/jackets'
   },
   {
     title: 'sneakers',
-    imageUrl: `https://i.ibb.co/0jqHpnp/sneakers.png`,
+    imageUrl: `https://i.imgur.com/hu2yVUx.jpg`,
     id: 3,
     linkUrl: 'shop/sneakers'
   },
   {
     title: 'womens',
-    imageUrl: `https://i.ibb.co/GCCdy8t/womens.png`,
+    imageUrl: `https://i.imgur.com/ph9sRAb.jpg`,
     size: 'large',
     id: 4,
     linkUrl: 'shop/womens'
   },
   {
     title: 'mens',
-    imageUrl: `https://i.imgur.com/YvmYfpu.png`,
+    imageUrl: `https://i.imgur.com/qxwQGpP.jpg`,
     size: 'large',
     id: 5,
     linkUrl: 'shop/mens',
